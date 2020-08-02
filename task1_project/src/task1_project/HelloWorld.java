e6c6d691808374d7bf7918922c4f60c3a369511d
Zanembheko <zanembhekon@gmail.com>
2020-08-02 03:20:30 +0200

    "Enter name"

--- task1_project/src/task1_project/HelloWorld.java
+++ task1_project/src/task1_project/HelloWorld.java
@@ -1,5 +1,18 @@
 package task1_project;
+import java.util.Scanner;
 
-public class HelloWorld {
+public class HelloWorld 
+{
+	
+		public static void main(String [] args)
+		{
+			System.out.println("Good day please enter your name:");
+			Scanner name = new Scanner(System.in);
+			String new_name = name.nextLine();
+			
+			System.out.println("Hello "+new_name+" Git is awesome!");
+			
+		}
+	
 
 }
